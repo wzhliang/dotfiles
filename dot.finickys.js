275d920e068dc18d1f548d89b1c326b8ad52f11b
Wenzhi Liang <wenzhi.liang@gmail.com>
2020-06-30 10:19:33 +0800

    vivaldi

--- dot.finickys.js
+++ dot.finickys.js
@@ -3,17 +3,22 @@ module.exports = {
   handlers: [
 	  {
 		  match: finicky.matchDomains([
-			  "www.yuque.com",
-			  "jira.wise2c.com",
 			  "ted.com",
 			  /.*\.medium.com$/,
 			  "stackoverflow.com",
 			  "dev.tencent.com",
 			  "tower.im",
 			  "mailing.dzone.com",
-			  "lanhuapp.com",
 		  ]),
 		  browser: "Safari"
+	  },
+    {
+		  match: finicky.matchDomains([
+			  "www.yuque.com",
+			  "jira.wise2c.com",
+			  "lanhuapp.com",
+		  ]),
+		  browser: "Vivaldi"
 	  }
   ]
 };
